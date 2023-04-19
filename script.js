445e62f2f8d407f166d6bf691775a7a769caf7d4
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-04-19 16:22:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ function getFormvalue() {
     //Write your code here
 	let f = document.getElementById('fname').value;
 	let l = document.getElementById('lname').value;
-	alert(`${f}${l}`)
+	alert(`${f} ${l}`)
 }
